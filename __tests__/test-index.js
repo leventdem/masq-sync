7aae384acf6d03e6cf3e2739aefac9762fff9ec1
Andrei Sambra <a.sambra@qwant.com>
2018-03-05 15:19:16 +0100

    made progress on test coverage

--- __tests__/test-index.js
+++ __tests__/test-index.js
@@ -1,10 +1,13 @@
 const socketClusterServer = require('socketcluster-server')
 const MasqSync = require('../src/index')
 
-const opts = {
+const options = {
   hostname: 'localhost',
-  port: 8008
+  port: 8008,
+  multiplex: false
 }
+
+const DEFAULTURL = 'http://localhost:8000/socketcluster/'
 
 let server
 let sockets = []
@@ -12,20 +15,20 @@ let peers = {}
 const nrPeers = 3
 
 // Start WebSocket server
-beforeAll(() => {
-  return new Promise((resolve) => {
-    server = socketClusterServer.listen(opts.port)
-    server.on('ready', async () => {
-      for (let i = 0; i < nrPeers; i++) {
-        sockets.push(new MasqSync())
-      }
-      // each peer has a list of IDs of other peers
-      sockets.forEach(async (socket) => {
-        peers[socket.ID] = sockets.map(peer => peer.ID).filter((peer) => peer !== socket.ID)
-        await socket.init(opts)
-        console.log('State:', socket.socket)
-      })
-      return resolve()
+beforeAll((done) => {
+  server = socketClusterServer.listen(options.port)
+  server.on('ready', async () => {
+    for (let i = 0; i < nrPeers; i++) {
+      sockets.push(new MasqSync())
+    }
+    // each peer has a list of IDs of other peers
+    let pending = []
+    sockets.forEach((socket) => {
+      peers[socket.ID] = sockets.map(peer => peer.ID).filter((peer) => peer !== socket.ID)
+      pending.push(socket.init(options))
+    })
+    Promise.all(pending).then(() => {
+      done()
     })
   })
 })
@@ -33,12 +36,19 @@ beforeAll(() => {
 // Close sockets & WebSocket server after all tests end
 afterAll(() => {
   sockets.forEeach((socket) => socket.destroy())
-  server.close()
+  // server.close()
 })
 
-describe('Test server', () => {
-  it('should be ready', (done) => {
+describe('Bootstrapping tests', () => {
+  it('should have connected the server', (done) => {
     expect(server.isReady).toBeTruthy()
+    done()
+  })
+
+  it('should have connected the clients', (done) => {
+    sockets.forEach((socket) => {
+      expect(socket.socket.state).toEqual('open')
+    })
     done()
   })
 })
@@ -49,7 +59,7 @@ describe('MasqSync', () => {
     configs.forEach(async (cfg) => {
       const socket = new MasqSync()
       await socket.init(cfg)
-      expect(socket.socket.clientId).toEqual('http://localhost:8000/socketcluster/')
+      expect(socket.socket.clientId).toEqual(DEFAULTURL)
     })
     done()
   })
@@ -60,8 +70,8 @@ describe('MasqSync', () => {
       try {
         const socket = new MasqSync()
         await socket.init(cfg)
-      } catch (e) {
-        expect(e.message).toMatch(/invalid/)
+      } catch (err) {
+        expect(err.message).toMatch(/invalid/)
       }
     })
     done()
@@ -74,8 +84,8 @@ describe('MasqSync', () => {
         const socket = new MasqSync()
         await socket.init(cfg)
         expect(socket.socket.state).toEqual(socket.socket.CLOSED)
-      } catch (e) {
-        expect(e.message).toMatch(/Socket hung up/)
+      } catch (err) {
+        expect(err.message).toMatch(/Socket hung up/)
       }
     })
     done()
@@ -100,13 +110,13 @@ describe('Peers', () => {
     done()
   })
 
-  it('should not subscribe to an invalid peer', (done) => {
+  it('should not subscribe to an invalid peer', async (done) => {
     const badValues = [ [], '', null, undefined ]
     badValues.forEach(async (val) => {
       try {
         await sockets[0].subscribePeer(val)
-      } catch (e) {
-        expect(e.message).toMatch(/invalid/i)
+      } catch (err) {
+        expect(err.message).toMatch(/invalid/i)
       }
     })
     done()
@@ -120,8 +130,8 @@ describe('Peers', () => {
     badValues.forEach(async (val) => {
       try {
         await sockets[0].subscribePeers(val)
-      } catch (e) {
-        expect(e.message).toMatch(/invalid/i)
+      } catch (err) {
+        expect(err.message).toMatch(/invalid/i)
       }
       expect(Object.keys(sockets[0].channels).length).toEqual(0)
     })
@@ -129,51 +139,65 @@ describe('Peers', () => {
     done()
   })
 
-  it('should subscribe to other peers', (done) => {
+  it('should subscribe to other peers', async (done) => {
     sockets.forEach(async (socket) => {
       await socket.subscribePeers(peers[socket.ID])
       expect(Object.keys(socket.channels).length).toEqual(nrPeers - 1)
-    })
-    done()
-  })
 
-  it('each peer should have individual channels with other peers', (done) => {
-    sockets.forEach((socket) => {
       peers[socket.ID].forEach((peer) => {
         expect(socket.channels[peer]).not.toBeUndefined()
+        // console.log(socket.channels[peer].socket)
         expect(socket.channels[peer].socket.state).toEqual(socket.channels[peer].socket.SUBSCRIBED)
       })
     })
     done()
   })
 
-  // it('should receive pings from other peers', async (done) => {
-  //   sockets[1].myChannel.watch((msg) => {
-  //     expect(msg.event).toEqual('ping')
-  //     expect(msg.from).toEqual(sockets[0].ID)
-  //     done()
-  //   })
-  //   sockets[0].channels[sockets[1].ID].socket.publish({
-  //     event: 'ping',
-  //     from: sockets[0].ID
-  //   })
-  // })
+  it('should receive pings from other peers', (done) => {
+    sockets[0].myChannel.watch((msg) => {
+      expect(msg.event).toEqual('ping')
+      expect(peers[sockets[0].ID]).toContain(msg.from)
+      done()
+    })
+    for (let i = 1; i < nrPeers; i++) {
+      peers[sockets[i].ID].forEach((peer) => {
+        sockets[i].channels[peer].socket.publish({
+          event: 'ping',
+          from: sockets[i].ID
+        })
+      })
+    }
+  })
 
   // it('should subscribe to new peers on pings', async (done) => {
-  //   const socket = new MasqSync()
-  //   await socket.init(opts)
+  //   try {
+  //     const socket = new MasqSync()
+  //     // await socket.init(options)
+  //     console.log(socket)
+  //   } catch (err) {
+  //     console.log(err)
+  //   }
+  //   // socket.init(options).then(() => {
+  //   //   console.log(socket)
+  //   //   // console.log(socket)
+  //   //   done()
+  //   // }).catch((err) => {
+  //   //   console.log(socket)
+  //   //   console.log(err)
+  //   //   done()
+  //   // })
+  //   // expect(socket.socket.state).toEqual('open')
 
-  //   sockets[0].myChannel.watch((msg) => {
-  //     expect(msg.event).toEqual('ping')
-  //     expect(msg.from).toEqual(socket.ID)
+  //   // sockets[0].myChannel.watch((msg) => {
+  //   //   expect(msg.event).toEqual('ping')
+  //   //   expect(msg.from).toEqual(socket.ID)
 
-  //     socket.close()
+  //   //   // socket.close()
 
-  //     done()
-  //   })
+  //   //   done()
+  //   // })
 
-  //   socket.subscribePeer(sockets[0].ID).then(() => {
-  //     expect(Object.keys(socket.channels).length).toEqual(1)
-  //   })
+  //   // await socket.subscribePeer(sockets[0].ID)
+  //   // expect(Object.keys(socket.channels).length).toEqual(1)
   // })
 })
